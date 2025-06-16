1c1a319a1f4a5a55a09659649711983883ddfe18
Joydeep Hans <144369315+JOY23072005@users.noreply.github.com>
2025-06-16 14:02:35 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,4 +3,7 @@ import axios from "axios";
 export const axiosInstances = axios.create({
   baseURL: import.meta.env.VITE_SERVER_URL,
   withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json',
+  }
 });
